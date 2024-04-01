44bc47cdad1854f4e61b18c187bcc257accf6873
JackieNeoCEG <e0959140@u.nus.edu>
2024-04-02 00:27:40 +0800

    Add Render.java class to OOPify Ui, fix gradlew bugs

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 
 public class Duke {
-    private static final Ui ui = new Ui();
+    private static final Render render = new Render();
+    private static final Ui ui = new Ui(render);
     //private static final HangMan hangman = new HangMan();
     private static ArrayList<Game> games = new ArrayList<>();
     private static int gameCounter = 0;
