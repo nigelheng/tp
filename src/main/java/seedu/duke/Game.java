6d4228fe5e7f22d6d1e9cf9ecdfc8204686195da
YoengKokLeong <e0968894@u.nus.edu>
2024-04-02 18:07:09 +0800

    edited TicTacToe and HangMan classes. Added documentation for Game class

--- src/main/java/seedu/duke/Game.java
+++ src/main/java/seedu/duke/Game.java
@@ -1,23 +1,38 @@
 package seedu.duke;
-
 import seedu.duke.exceptions.InvalidTTMoveException;
 
+/**
+ * Game class with 2 subclasses TicTacToe and HangMan
+ * Contains methods for handling running of game and showing of game guide
+ */
 public class Game {
     protected boolean isExit; // true to exit
     protected String gameName;
 
+    /**
+     * creates a game
+     *
+     * @param line is used to set the gameName of the game
+     */
     public Game(String line) {
         gameName = line;
         isExit = false; //not used for nw
         assert !isExit; //not used for now
     }
-    public void runTicTacToe() throws InvalidTTMoveException {
+
+    /**
+     * runs game for respective class.
+     * overridden in TicTacToe and HangMan
+     * @throws InvalidTTMoveException
+     */
+    public void runGame() throws InvalidTTMoveException {
 
     }
 
-    public void runHangMan() {
-    }
-
+    /**
+     * displays game guide when 'guide' is inputted
+     * overridden in TicTacToe and Hangman
+     */
     public void howToPlay() {
         System.out.println("----------------------------------------------------");
         System.out.println("Let me explain the rules of this game:");
--- src/main/java/seedu/duke/TicTacToe.java
+++ src/main/java/seedu/duke/TicTacToe.java
@@ -66,7 +66,7 @@ public class TicTacToe extends Game {
         return "unending";
     }
 
-    @Override public void runTicTacToe() {
+    @Override public void runGame() {
         for (int i = 0; i < 9; i++) {
             board[i] = " ";
         }
@@ -146,6 +146,9 @@ public class TicTacToe extends Game {
         }
     }
 
+    /**
+     * Prints the game guide when 'guide' is inputted
+     */
     @Override public void howToPlay() {
         super.howToPlay();
         System.out.println("\t- Tic-Tac-Toe is an 1 VS 1 game. Hence, you will be challenging ME! HAHAHA");
